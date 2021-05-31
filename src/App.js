8d726d4e9f99402ff20772e085297f2d336790c6
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-05-31 00:44:22 -0700

    commit tree f8b277edf6bf59723e8e2660e7550e5e48c3bf71

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import CatList from './CatList'
+import { fetchCats } from './actions/catActions'
 
-class App extends Component {   
+class App extends Component {  
+  
+  componentDidMount() {
+    console.log(this.props)
+    this.props.fetchCats()
+  }
+
+  renderLoading() {
+    if (this.props.loading) {
+      return <h4>Loading...</h4>
+    }
+  }
   
   render() {
+    console.log(this.props.catPics)
     return (
-      <div>
+      <div className="App">
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        {this.renderLoading()}
+        <CatList catPics={this.props.catPics}/>
       </div>
     );
   }
 }
 
-export default App
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
